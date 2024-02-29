23f1b037b1769daf315cb2d0bb04fdbf39604fb9
kumarpremsathya <Premkumar.karuppusamy@mnw.net.in>
2024-02-29 17:18:33 +0530

    mca models has been removed

--- Sebi_apiproject/Sebi_app/views.py
+++ Sebi_apiproject/Sebi_app/views.py
@@ -1,4 +1,4 @@
-12344444from rest_framework.response import Response
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
